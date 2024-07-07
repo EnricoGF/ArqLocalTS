0c1c1558d96b80a461b337dcea197657fdb29d66
EnricoGF <166620038+EnricoGF@users.noreply.github.com>
2024-07-07 12:03:05 -0300

    [new] RPG game

---
+++ RPGgame/script.ts
@@ -0,0 +1,90 @@
+/* class Coruja {
+    nome:string
+    cor:string
+    voa:boolean
+    chirria:boolean
+
+    constructor (owlNome:string, owlCor:string, owlVoa:boolean, owlChirria:boolean) {
+        this.nome = owlNome
+        this.cor = owlCor
+        this.voa = owlVoa
+        this.chirria =owlChirria
+    }
+
+    voar():string {
+        let corujaVoa:string = ``
+        if (this.voa == true){
+            corujaVoa= `A coruja esta voando`
+        }
+        else if (this.voa == false) {
+            corujaVoa = `A coruja nao pode voar`
+        }
+        return corujaVoa
+    }
+
+    infos():string {
+        return `O nome da coruja eh ${this.nome}, ela eh ${this.cor}, e ela voa: ${this.voa}`
+    }
+
+    som():string{
+        let corujaChirria:string = ``
+        if(this.chirria == true){
+            corujaChirria = `A coruja esta chirriando`
+        }
+        else if (this.chirria == false) {
+            corujaChirria = `A coruja nao pode chirriar`
+        }
+        return corujaChirria
+    }
+}
+
+const minhaCoruja:Coruja = new Coruja(`Carlos`, `Branca`, false, true)
+
+console.log(minhaCoruja.infos())
+console.log(minhaCoruja.som())
+console.log(minhaCoruja.voar())
+ */
+
+class Player {
+    playerName:string
+    playerAttack:number
+    playerHealth:number
+
+    constructor(name:string, health:number, attack:number){
+        this.playerName = name
+        this.playerHealth = health
+        this.playerAttack = attack
+    }
+
+    playerTookDamageFunc():number{
+        return this.playerHealth - playerTookDamageNumber
+    }
+
+    playerDealDamage():number{
+        return this.playerAttack
+    }
+
+}
+
+class Enemy{
+    enemyAttack:number
+    enemyHealth:number
+
+    constructor(attack:number, health:number){
+        this.enemyAttack = attack
+        this.enemyHealth = health
+    }
+
+    enemyAttackPlayerFunc():number{
+        return this.enemyAttack
+    }
+
+    enemyHealthFunc():number{
+        return this.enemyHealth - playerDealDamageNumber
+    }
+}
+const enemyInstance:Enemy = new Enemy(10,100)
+const playerInstance:Player = new Player(`Enrico`, 12, 80)
+
+const playerTookDamageNumber = enemyInstance.enemyAttackPlayerFunc()
+const playerDealDamageNumber = playerInstance.playerDealDamage()
